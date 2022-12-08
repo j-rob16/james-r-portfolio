8d57a641a4009628a94776f782cd5de2bcd73d56
James R <jimmer1609@gmail.com>
2022-12-08 21:50:38 +0900

    mobile menu icons added

--- src/components/MarkerArrow.jsx
+++
@@ -1,9 +0,0 @@
-const MarkerArrow = () => {
-  return (
-    <div className='border-solid border-black border-tr-[3px]'>
-
-    </div>
-  );
-};
-
-export default MarkerArrow;
--- src/scenes/NavBar.jsx
+++ src/scenes/NavBar.jsx
@@ -121,30 +121,48 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage, mode, setMode, lan
 
             {/* MENU ITEMS */}
             <div className={`${languageSet[language].font} flex flex-col gap-10 ml-[33%] text-2xl`}>
-              <Link 
-                page='Home'
-                title={languageSet[language].home}
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link 
-                page='About'
-                title={languageSet[language].about}
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link 
-                page='Projects'
-                title = {languageSet[language].projects}
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
+              <div className='flex flex-row items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
 
-              {/* <div >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <Link 
+                  page='Home'
+                  title={languageSet[language].home}
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+
+              </div>
+
+              <div className='flex flex-row items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                </svg>
+                <Link 
+                  page='About'
+                  title={languageSet[language].about}
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+
+              <div className='flex flex-row items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                </svg>
+                <Link 
+                  page='Projects'
+                  title = {languageSet[language].projects}
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+
+              <div className='flex flex-row items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-                </svg> */}
-
+                </svg>
                 <Link 
                   page='Contact'
                   title={languageSet[language].contact}
@@ -152,7 +170,7 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage, mode, setMode, lan
                   setSelectedPage={setSelectedPage}
                 />
 
-              {/* </div> */}
+              </div>
             </div>
 
           </div>
